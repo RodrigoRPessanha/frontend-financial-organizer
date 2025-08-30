90c6e898f24b82553896c03de8d7ed89cee7ac52
Rodrigo Pessanha <rodrigorodrigues.sp@outlook.com>
2025-08-30 16:26:27 -0300

    feat: enhance useSession hook to include forceGuest functionality

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ function formatDateBR(dateStr: string) {
 }
 
 export default function Page() {
-  const { loading, loggedIn, username, check, logout } = useSession();
+  const { loading, loggedIn, username, check, logout, forceGuest} = useSession();
 
   const [cats, setCats] = useState<Cat[]>([]);
   const [subs, setSubs] = useState<Sub[]>([]);
